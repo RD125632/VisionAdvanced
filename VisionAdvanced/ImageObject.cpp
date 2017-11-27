10d7fdf4ad099d770019a0cad27f58670af8567c
DESKTOP-1B7KFML\Richard Danen <rcajdane@avans.nl>
2017-11-27 12:29:19 +0100

    Fixed bugs in getContour

--- VisionAdvanced/ImageObject.cpp
+++ VisionAdvanced/ImageObject.cpp
@@ -4,12 +4,13 @@
 
 void ImageObject::findContour(Mat binary_image, Point2d firstPixel)
 {
-	//Add first pixel
-	contour.push_back(firstPixel);
-
-	Point2d currentPixel = firstPixel;
+	int row = firstPixel.x;
+	int colmn = firstPixel.y;
 	int dir = 3;
 	int arr_i = -1;
+	vector<int> chain_code;
+	vector<Point> sub_contour;
+
 	int offset[8][2] = {
 		{ -1, -1 },
 		{ -1, 0 },
@@ -21,35 +22,42 @@ void ImageObject::findContour(Mat binary_image, Point2d firstPixel)
 		{ 0, -1 },
 	};
 
-	// Find the chaincode
-	for (int index = 0; index < 8; index++) {
-		if (index == 0) {
-			arr_i = dir - 2;
-			if (arr_i == -1) { arr_i = 7; }
-			if (arr_i == -2) { arr_i = 6; }
+	while (true) {
+		for (int index = 0; index < 8; index++) {
+			if (index == 0) {
+				arr_i = dir - 2;
+				if (arr_i == -1) {
+					arr_i = 7;
+				}
+				if (arr_i == -2) {
+					arr_i = 6;
+				};
+			}
+			if (arr_i == 8) { arr_i = 0; }
+			if (binary_image.at<__int16>(row + offset[arr_i][0], colmn + offset[arr_i][1]) > 0) {
+				if (arr_i == 0) { chain_code.push_back(3); }
+				if (arr_i == 1) { chain_code.push_back(2); }
+				if (arr_i == 2) { chain_code.push_back(1); }
+				if (arr_i == 3) { chain_code.push_back(0); }
+				if (arr_i == 4) { chain_code.push_back(7); }
+				if (arr_i == 5) { chain_code.push_back(6); }
+				if (arr_i == 6) { chain_code.push_back(5); }
+				if (arr_i == 7) { chain_code.push_back(4); }
+
+				sub_contour.push_back(Point(row, colmn));
+				row = row + offset[arr_i][0];
+				colmn = colmn + offset[arr_i][1];
+				dir = arr_i;
+				break;
+			}
+			arr_i++;
 		}
-		if (arr_i == 8) { arr_i = 0; }
-
-		double newX = currentPixel.x + offset[arr_i][0];
-		double newY = currentPixel.y + offset[arr_i][1];
-
-		if (binary_image.at<__int16>(currentPixel.y + offset[arr_i][1], currentPixel.x + offset[arr_i][0]) > 0) {
-			if (arr_i == 0) { chainCode.push_back(3); }
-			if (arr_i == 1) { chainCode.push_back(2); }
-			if (arr_i == 2) { chainCode.push_back(1); }
-			if (arr_i == 3) { chainCode.push_back(0); }
-			if (arr_i == 4) { chainCode.push_back(7); }
-			if (arr_i == 5) { chainCode.push_back(6); }
-			if (arr_i == 6) { chainCode.push_back(5); }
-			if (arr_i == 7) { chainCode.push_back(4); }
-
-			currentPixel.x = newX;
-			currentPixel.y = newY;
-			dir = arr_i;
+		if (colmn == firstPixel.y && row == firstPixel.x) {
 			break;
 		}
-		arr_i++;
 	}
+	contour = sub_contour;
+	chainCode = chain_code;
 }
 
 void ImageObject::findBendingEnergy()
