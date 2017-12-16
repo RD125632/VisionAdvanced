f927210b10b883991eafb15c04588c54697282b5
Richard Danen <rcajdane@avans.nl>
2017-12-16 13:07:04 +0100

    Fase 3: Jans code werkend gekregen

--- Fase 3 - Neural Network/main.cpp
+++ Fase 3 - Neural Network/main.cpp
@@ -2,12 +2,14 @@
 // Jan Oostindie, Avans Hogeschool, dd 6-12-2016
 // email: jac.oostindie@avans.nl
 
+#include "stdafx.h"
+#include "avansvisionlib20.h" // versie 2.0 (!)
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include <opencv/cv.h>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
+#include <iomanip>
 #include <string>
-#include "avansvisionlib20.h" // versie 2.0 (!)
 
 using namespace cv;
 using namespace std;
